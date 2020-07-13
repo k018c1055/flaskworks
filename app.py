6205b698d12dfe7e349d171d6584aaf060da7ed8
k018c1055 <k018c1055@it-neec.jp>
2020-07-13 12:10:21 +0900

    3.2.template(kadai3-2)

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html',num = 7)
+    return render_template('index.html', list={"英語": 87, "数学": 90, "国語": 45, "理科": 76, "社会": 31})
 
 
 if __name__ == '__main__':
