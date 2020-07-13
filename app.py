cc050f85301d30ac39ba35b19e09cd78ded71448
k018c1055 <k018c1055@it-neec.jp>
2020-07-13 11:46:42 +0900

    3.2.template(dic)

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
-    return render_template('index.html',title="タイトル")
+    return render_template('index.html',obj={"title":"タイトル2"})
 
 
 if __name__ == '__main__':
